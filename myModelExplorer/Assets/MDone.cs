345831e1bb53426fa7c52ff8ecf944a7cd5fdaa0
wdear <wdear0401@163.com>
2017-03-24 11:19:24 +0800

    backup

---
+++ myModelExplorer/Assets/MDone.cs
@@ -0,0 +1,30 @@
+﻿using Academy.HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MDone : MonoBehaviour {
+
+    public List<GameObject> restartObj;
+	// Use this for initialization
+	void Start () {
+        Invoke("restartM", 5);
+        Destroy(this.gameObject, 5);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void restartM()
+    {
+        GameObject.Find("rack").GetComponent<testPUI>().enabled = true;
+        foreach(GameObject x in restartObj)
+        {
+            x.SetActive(true);
+        }
+
+    }
+}
---
+++ myModelExplorer/Assets/lineBlue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineBlue : MonoBehaviour {
+
+    public GameObject preMs;
+    public GameObject errorMs;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTap()
+    {
+        errorMs.SetActive(true);
+        preMs.SetActive(false);
+        Invoke("popMs", 2);
+    }
+
+    void popMs()
+    {
+        errorMs.SetActive(false);
+        preMs.SetActive(true);
+    }
+}
